43a65679197fce65dd41a26acc14d9fdddb3a78f
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-05-25 15:29:19 +0530

    question2

---
+++ dist/Assignments/createpath.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = __importDefault(require("path"));
+const Projectpath = path_1.default.join("projects", "node.js", "src", "index.js");
+console.log(Projectpath);
---
+++ src/Assignments/createpath.ts
@@ -0,0 +1,9 @@
+import path from "path";
+const Projectpath=path.join(
+    "projects",
+    "node.js",
+    "src",
+    "index.js"
+
+);
+console.log(Projectpath);
