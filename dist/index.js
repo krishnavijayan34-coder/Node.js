b445d9cbe9d8a829bea17649ded7e11c4209184a
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-05-25 11:06:12 +0530

    importing Json and watch mode

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,5 @@
 "use strict";
-//require("./batman");
-//require("./superman");
-// import SuperHero from "./modulecatching"
-//  console.log(SuperHero.getName());
-//  SuperHero.setName("Superman")
-// console.log(SuperHero.getName())
-Object.defineProperty(exports, "__esModule", { value: true });
-// import NewSuperHero from "./modulecatching";
-// console.log(NewSuperHero.getName());
-const math = require("./math");
-const { add, subtract } = math;
-console.log(math.add(2, 3));
-console.log(math.subtract(2, 3));
+// import copyData from "./copydata.json"
+// console.log(copyData.name);
+//watch mode
+console.log("Hlo krisha ");
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
-//require("./batman");
-//require("./superman");
-// import SuperHero from "./modulecatching"
-//  console.log(SuperHero.getName());
-//  SuperHero.setName("Superman")
-// console.log(SuperHero.getName())
+// import copyData from "./copydata.json"
 
-// import NewSuperHero from "./modulecatching";
-
-// console.log(NewSuperHero.getName());
+// console.log(copyData.name);
 
 
-const math = require("./math");
-
-const {add,subtract}=math;
-
-console.log(math.add(2,3));
-console.log(math.subtract(2,3));
-
-export {};
+//watch mode
+console.log("Hlo krisha ");
