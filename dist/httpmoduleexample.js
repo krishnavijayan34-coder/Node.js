95e2b3ef355f0d03f5899b570966b5b43a893107
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-05-27 14:05:58 +0530

    example http module

--- dist/httpmoduleexample.js
+++ dist/httpmoduleexample.js
@@ -6,7 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
 const server = http_1.default.createServer((req, res) => {
     //set response header
-    res.writeHead(200, {
-        "content-Type": "text/plain"
-    });
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World!");
+});
+//3000 is port number
+server.listen(3000, () => {
+    console.log("server running on port 3000");
 });
--- src/httpmoduleexample.ts
+++ src/httpmoduleexample.ts
@@ -1,7 +1,10 @@
 import http from "http";
 const server = http.createServer((req,res)=> {
     //set response header
-    res.writeHead(200, {
-        "content-Type":"text/plain"
+    res.writeHead(200, {"Content-Type":"text/plain"});
+    res.end("Hello World!");
     });
-})
+//3000 is port number
+    server.listen(3000,()=>{
+        console.log("server running on port 3000");
+    });
