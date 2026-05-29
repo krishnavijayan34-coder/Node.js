8083a268a23a68ea58b646aeac84da8464a7189b
Krishna Vijayan <krishnavijayan34@gmail.com>
2026-05-29 11:48:35 +0530

    i/o polling example

--- dist/I/OQueue.js
+++ dist/I/OQueue.js
@@ -11,6 +11,7 @@ fs_1.default.readFile(__filename, () => {
 process.nextTick(() => console.log("this is process.nextTick 1"));
 Promise.resolve().then(() => console.log("this is promise.resolve 1"));
 setTimeout(() => console.log("this is setTimeout 1"), 0);
+setImmediate(() => console.log("this is setImmediate 1"));
 //experiment 2
 //   import fs from "fs";
 //   setTimeout(() => console.log("this is setTimeout 1"),0);
--- src/I/OQueue.ts
+++ src/I/OQueue.ts
@@ -6,6 +6,7 @@ fs.readFile(__filename,() => {
    process.nextTick(()=> console.log("this is process.nextTick 1"));
    Promise.resolve().then(()=> console.log("this is promise.resolve 1"));
    setTimeout(() => console.log("this is setTimeout 1"),0);
+   setImmediate(() => console.log("this is setImmediate 1"));
 
 //experiment 2
 
